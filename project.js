f84809f307395fe4f653f97a7fc2d0ea2d729927
CADBOT <xdothackerx@gmail.com>
2015-06-10 14:44:56 -0700

    add a happy chapter!

--- project.js
+++ project.js
@@ -1,3 +1,6 @@
 console.log("Goodbye sad world")
 console.log("The world... blah blah blah")
 console.log("Somthing about javascript...")
+
+console.log('All of the sudden the world became a better place')
+console.log('and we are all happy again!')
