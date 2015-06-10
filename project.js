3ba729ee264f1db70c99f64209f52ba352d9f063
CADBOT <xdothackerx@gmail.com>
2015-06-10 14:41:27 -0700

    add new chapter

--- project.js
+++ project.js
@@ -1,3 +1,7 @@
 console.log("Goodbye sad world")
 console.log("The world... blah blah blah")
 console.log("Somthing about javascript...")
+
+
+console.log("I resolved myself to find some dynamite")
+console.log("With it I can destroy this acursed world")
